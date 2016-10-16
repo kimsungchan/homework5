5072a864654417e77f5bd3ebb6c5f39f964b7399
김성찬 <dlrj1234@naver.com>
2016-10-16 10:24:13 +0900

    정사각혐 calss 만들기 완료

--- homework5/src/homework5/RectagleStars.java
+++ homework5/src/homework5/RectagleStars.java
@@ -1,7 +1,19 @@
 package homework5;
+import java.util.*;
 
 public class RectagleStars {
      public static void Rectag(){
+    	 Scanner s = new Scanner(System.in);
     	 
+    	 System.out.println("============================");
+    	 System.out.println("정사각형 크기입력 : ");
+    	 int square = s.nextInt();
+    	 
+    	 for(int i=0 ; i < square ; i++){
+    		 for(int j =0 ; j < square ; j++){
+    		 System.out.print("*");
+    		 }
+    		 System.out.println();  	 
+    	 } 
      }
 }
