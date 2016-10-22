2cc228760468d40a7f0cdeb5172134354899fac6
김성찬 <dlrj1234@naver.com>
2016-10-22 15:24:38 +0900

    마름모 수정

--- homework5/src/homework5/IsoscelesTriangleStars2.java
+++ homework5/src/homework5/IsoscelesTriangleStars2.java
@@ -1,7 +1,38 @@
 package homework5;
+import java.util.Scanner;
 
 public class IsoscelesTriangleStars2 {
     public static void ISO2(){
-    	
-    }
+    	Scanner s = new Scanner(System.in);
+   	 
+   	      System.out.println("============================");
+   	      System.out.print("다이아몬드의 크기입력 : ");
+   	      int square = s.nextInt();
+   	 
+   	        for(int i=0 ; i < square ; i++){
+   		     for(int j =i ; j < square ; j++){
+   		        System.out.print(" ");
+             }
+   		     for(int j = 0 ; j < i ; j++){
+   		    	 System.out.print("*");
+   		     }
+   		     for(int j = 0 ; j< i-1 ; j++){
+   		    	 System.out.print("*");
+   		     }
+   		         System.out.print("\n");
+   	    } 
+   	        
+   	         for(int i = 0 ; i < square ; i++){
+   	           for(int j = 0 ; j < i ; j++){ 
+   	             System.out.print(" ");
+   	           }
+   	           for(int j = i ; j < square ; j++){
+   	        	   System.out.print("*");
+   	           }
+   	           for(int j = i + 1  ; j < square ; j++){
+   	        	   System.out.print("*");
+   	           }
+   	           System.out.print("\n");
+   	    }      
+   }
 }
