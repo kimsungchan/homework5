e95df061a6d92c658659c6f1b2e50498550f519c
김성찬 <dlrj1234@naver.com>
2016-10-22 15:02:51 +0900

    이등변삼각형 수정

--- homework5/src/homework5/TriangleStars.java
+++ homework5/src/homework5/TriangleStars.java
@@ -7,15 +7,18 @@ public class TriangleStars {
          Scanner s = new Scanner(System.in);
     	 
     	 System.out.println("============================");
-    	 System.out.println("직각삼각형의 왼쪽여백과 높이입력  : ");
+    	 System.out.print("직각삼각형의 왼쪽여백과 높이입력  : ");
+    	 int back = s.nextInt();
     	 int square = s.nextInt();
-    	 
-    	 for(int i=0 ; i < square ; i++){
-    		 for(int j =0 ; j < square ; j++){
-    		 System.out.print("*");
+    	 for(int i= 0 ; i < square ; i++){
+    		 for(int j = 0 ; j < back-square-i ; j++){
+    		    	 System.out.print(" ");
+    		 } 	 
+    		 for(int j = 0 ; j < i+1 ; j++){
+    			 System.out.print("*");
     		 }
-    		 System.out.println();  	 
-    	 } 
+    		     System.out.print("\n");
+    	 }
      }
    }
 
